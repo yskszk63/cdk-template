8aa37172ef98d56f63fc22fbf329039b35f9bec4
yskszk63 <yskszk63@gmail.com>
2022-11-17 22:13:44 +0900

    Update jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
-};
+  },
+}
+
+export default config;
